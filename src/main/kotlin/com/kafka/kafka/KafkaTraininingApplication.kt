23fa29c2a8483a2e3f771d4e66b83fe336c80a42
Diego Mesquita de Jesus <diego.mesquita.jesus@gmail.com>
2024-11-30 21:12:48 -0300

    feat: Kafka training
    First commit.

---
+++ src/main/kotlin/com/kafka/kafka/KafkaTraininingApplication.kt
@@ -0,0 +1,11 @@
+package com.kafka.kafka
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KafkaTraininingApplication
+
+fun main(args: Array<String>) {
+	runApplication<KafkaTraininingApplication>(*args)
+}
